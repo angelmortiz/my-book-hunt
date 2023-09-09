dd2871a6d7aa98bc6f91ca31559658acfd8335ba
Angel Ortiz <angelmanuelortiz@gmail.com>
2023-09-09 18:39:45 -0500

    [frontend] Adding home page and a basic search bar component

---
+++ frontend/src/components/SearchBar.tsx
@@ -0,0 +1,27 @@
+import React, {useState} from 'react';
+import { HiMagnifyingGlass } from 'react-icons/hi2'
+
+const SearchBar: React.FC<{ onSearch: (query: string) => void }> = ({onSearch}) => {
+    const [searchTerm, setSearchTerm] = useState('');
+
+    const handleSearch = () => {
+        onSearch(searchTerm);
+    };
+
+    return (
+        <div className="flex items-center justify-center p-4">
+            <input
+                className="mr-2 w-full rounded-full border-2 border-cyan-600 px-4 py-2"
+                type="text"
+                placeholder="Search by titles, authors, ISBNs..."
+                value={searchTerm}
+                onChange={(e) => setSearchTerm(e.target.value)}
+            />
+            <button className="rounded-full bg-cyan-600 px-3 py-3 text-stone-100" onClick={handleSearch}>
+                <HiMagnifyingGlass />
+            </button>
+        </div>
+    );
+};
+
+export default SearchBar;
