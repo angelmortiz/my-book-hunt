4f617b4c0397da09acf3cb994cd6e37dc5088364
Angel Ortiz <angelmanuelortiz@gmail.com>
2023-09-10 08:11:30 -0500

    [frontend] Improving styling on BooksCard

--- frontend/src/components/BookCard.tsx
+++ frontend/src/components/BookCard.tsx
@@ -1,22 +1,33 @@
 import React from 'react';
 import {GoogleBookLiteResponse} from "@/types/books";
 
-const BookCard: React.FC<GoogleBookLiteResponse> = ({id, selfLink, volumeInfo }) => {
-    const {title, authors, publishedDate, imageLinks} = volumeInfo;
-    const getYear = (): string | undefined  => {
+const BookCard: React.FC<GoogleBookLiteResponse> = ({id, selfLink, volumeInfo}) => {
+    let {title, authors, publishedDate, imageLinks, categories} = volumeInfo;
+
+    const shortenTitle = (): string | undefined => {
+        return title.length < 48 ? title : title.substring(0, 48) + "...";
+    }
+
+    const getPublicationYear = (): string | undefined => {
         if (!publishedDate) return publishedDate;
 
         return new Date(publishedDate).getFullYear().toString()
     }
 
     return (
-        <div className="m-2 flex flex-col gap-1 rounded-md border bg-stone-50 p-4 shadow-md">
+        <div className="m-2 flex flex-col justify-between rounded-2xl border bg-stone-50 p-4 shadow-md text-al">
             <div className="mx-auto">
-                <img src={imageLinks?.thumbnail} alt={title} className="mb-4 h-52 w-auto rounded-md object-cover" />
+                <img src={imageLinks?.thumbnail} alt={title} className="mb-4 h-52 w-auto rounded-md object-cover"/>
             </div>
-            <h2 className="text-lg font-semibold text-cyan-700">{title}</h2>
-            <p className="text-stone-600">{authors[0]}</p>
-            <p className="text-stone-600">Publication year: {getYear()}</p>
+            <div className="flex h-48 flex-col justify-between">
+                <h2 className="text-center text-lg font-bold text-cyan-700 leading-tight">{shortenTitle()}</h2>
+                <div className="px-2">
+                    {authors && authors[0] && <p className="text-stone-600"><b>Author:</b> {authors[0]}</p>}
+                    {publishedDate && <p className="text-stone-600"><b>Year:</b> {getPublicationYear()}</p>}
+                    {categories && categories[0] && <p className="text-stone-600"><b>Category:</b> {categories[0].split(' & ')[0]}</p>}
+                </div>
+                <button className="rounded-full bg-cyan-600 p-2 text-stone-50">View Details</button>
+            </div>
         </div>
     );
 };
--- frontend/src/components/Grid.tsx
+++ frontend/src/components/Grid.tsx
@@ -8,7 +8,7 @@ interface GridProps {
 
 const Grid: React.FC<GridProps> = ({ books }) => {
     return (
-        <div className="mt-2 grid grid-cols-1 gap-4 sm:mt-4 sm:grid-cols-2 md: md:mt-6 md:grid-cols-3 lg:mt-8 lg:grid-cols-4 xl:grid-cols-5">
+        <div className="mt-2 grid grid-cols-1 gap-4 sm:mt-4 sm:grid-cols-2 md: md:mt-6 lg:mt-8 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5">
             {books?.map((book, index) => (
                 <BookCard key={index} {...book} />
             ))}
